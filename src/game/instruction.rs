495f5b5e1220a564fa4e2f89d7ed55f3c3df2fe1
Morgan Fogg <morganfogg@users.noreply.github.com>
2019-09-28 10:17:20 +1000

    Implemented print opcode

--- src/game/instruction.rs
+++ src/game/instruction.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 
 use crate::game::operand::Operand;
 use crate::game::routine::Routine;
+use crate::game::cursor::Cursor;
 
 pub enum Instruction {
     Normal(&'static dyn Fn(&mut Routine, Vec<Operand>) -> InstructionResult),
@@ -27,6 +28,7 @@ pub struct InstructionSet {
 impl InstructionSet {
     pub fn new(version: u8) -> InstructionSet {
         let mut instructions = HashMap::new();
+        instructions.insert(178, Instruction::StringLiteral(&common::print));
         instructions.insert(186, Instruction::Normal(&common::quit));
         if version >= 4 {
             instructions.insert(224, Instruction::Return(&common::call_vs));
@@ -41,10 +43,15 @@ impl InstructionSet {
 }
 
 mod common {
-    use super::{InstructionResult, Operand, Routine};
+    use super::{InstructionResult, Operand, Routine, Cursor};
 
     pub fn quit(_: &mut Routine, _: Vec<Operand>) -> InstructionResult {
         InstructionResult::Quit
+    }
+    
+    pub fn print(routine: &mut Routine, string: String) -> InstructionResult {
+        println!("print called with {}", string);
+        InstructionResult::Continue
     }
 
     pub fn call_vs(routine: &mut Routine, ops: Vec<Operand>, result: u8) -> InstructionResult {
