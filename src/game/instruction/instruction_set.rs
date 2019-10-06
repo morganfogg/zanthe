fb7627d1d1097307ebf3d0f0385aba09d1a1909a
Morgan Fogg <morganfogg@users.noreply.github.com>
2019-10-06 15:59:36 +1000

    Added store instruction

--- src/game/instruction/instruction_set.rs
+++ src/game/instruction/instruction_set.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::convert::TryInto;
 use std::error::Error;
 
 use itertools::Itertools;
@@ -14,6 +15,7 @@ pub struct InstructionSet {
 impl InstructionSet {
     pub fn new(version: u8) -> InstructionSet {
         let mut instructions: HashMap<u8, Instruction> = [
+            (13, Instruction::Normal(&common::store)),
             (141, Instruction::Normal(&common::print_paddr)),
             (176, Instruction::Normal(&common::rtrue)),
             (177, Instruction::Normal(&common::rfalse)),
@@ -25,7 +27,7 @@ impl InstructionSet {
         .collect();
         if version >= 4 {
             instructions.extend(
-                [(224, Instruction::Return(&version_gte4::call_vs))]
+                [(224, Instruction::Store(&version_gte4::call_vs))]
                     .iter()
                     .cloned()
                     .collect::<HashMap<u8, Instruction>>(),
@@ -45,7 +47,6 @@ impl InstructionSet {
     }
 
     pub fn get(&self, opcode: u8) -> Option<&Instruction> {
-        info!("Code: {0:x} ({0})", opcode);
         self.instructions.get(&opcode)
     }
 }
@@ -53,23 +54,46 @@ impl InstructionSet {
 mod common {
     use super::*;
 
+    /// 2OP:13 Set the variable referenced by the operand to value
+    pub fn store(
+        mut context: Context,
+        ops: Vec<Operand>,
+    ) -> Result<InstructionResult, Box<dyn Error>> {
+        let variable: u8 = ops[0]
+            .get_value(&mut context)?
+            .ok_or_else(|| GameError::InvalidOperation("Missing required operand".into()))?
+            .try_into()
+            .map_err(|_| GameError::InvalidOperation("Invalid variable number".into()))?;
+        let value = ops[1]
+            .get_value(&mut context)?
+            .ok_or_else(|| GameError::InvalidOperation("Missing required operand".into()))?;
+
+        context.set_variable(variable, value);
+        Ok(InstructionResult::Continue)
+    }
+
+    /// 0OP:176 Returns true (1).
     pub fn rtrue(_: Context, _: Vec<Operand>) -> Result<InstructionResult, Box<dyn Error>> {
         Ok(InstructionResult::Return(1))
     }
 
+    /// 0OP:177 Returns false (0).
     pub fn rfalse(_: Context, _: Vec<Operand>) -> Result<InstructionResult, Box<dyn Error>> {
         Ok(InstructionResult::Return(0))
     }
 
+    /// 0OP:186 Exits the game.
     pub fn quit(_: Context, _: Vec<Operand>) -> Result<InstructionResult, Box<dyn Error>> {
         Ok(InstructionResult::Quit)
     }
 
+    /// 0OP:178 Prints a string stored immediately after the instruction.
     pub fn print(context: Context, string: String) -> Result<InstructionResult, Box<dyn Error>> {
         context.interface.print(&string)?;
         Ok(InstructionResult::Continue)
     }
 
+    /// 1OP:114 Prints a string stored at a padded address.
     pub fn print_paddr(
         mut context: Context,
         ops: Vec<Operand>,
@@ -87,6 +111,8 @@ mod common {
 
 mod version_gte4 {
     use super::*;
+
+    /// VAR:224 Calls a routine with up to 3 arguments and stores the result.
     pub fn call_vs(
         mut context: Context,
         ops: Vec<Operand>,
@@ -115,6 +141,8 @@ mod version_gte4 {
 
 mod version_gte5 {
     use super::*;
+
+    /// Calls a routine with no arguments and throws away the result.
     pub fn call_1n(
         mut context: Context,
         ops: Vec<Operand>,
